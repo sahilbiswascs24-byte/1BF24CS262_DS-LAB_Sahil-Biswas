a7079115b85cc78eff3d08d6b347a28d758e544d
Sahil Biswas <sahilbiswas.cs24@bmsce.ac.in>
2025-12-22 09:29:05 +0530

    Add files via upload

---
+++ Lab Program 9/9b.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#define MAX 20
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n;
+
+void DFS(int v)
+{
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (adj[v][i] == 1 && !visited[i])
+        {
+            DFS(i);
+        }
+    }
+}
+
+int main()
+{
+    int connected = 1;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = 0;
+    }
+
+    DFS(0);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!visited[i])
+        {
+            connected = 0;
+            break;
+        }
+    }
+
+    if (connected)
+        printf("The graph is CONNECTED.\n");
+    else
+        printf("The graph is NOT CONNECTED.\n");
+
+    return 0;
+}
+
